0155fe23f90a4896038313c29056ae67c90b80bb
Dmitri Pisarenko <dp@altruix.co>
2017-01-11 18:48:35 +0300

    Attempt at bugfix (CORS).

--- src/main/java/maven/Main.java
+++ src/main/java/maven/Main.java
@@ -16,6 +16,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(MavenController.class, args);
     }
+/*
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
@@ -28,4 +29,5 @@ public class Main {
             }
         };
     }
+    */
 }
