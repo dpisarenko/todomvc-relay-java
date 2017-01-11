f574360adce5bb5c865397ba0d6a30f071fad4d5
Dmitri Pisarenko <dp@altruix.co>
2017-01-11 13:58:50 +0300

    Initial commit.

---
+++ src/main/java/maven/Main.java
@@ -0,0 +1,13 @@
+package maven;
+
+import org.springframework.boot.SpringApplication;
+import todomvc.GraphQLController;
+
+/**
+ * Created by pisarenko on 11.01.2017.
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MavenController.class, args);
+    }
+}
