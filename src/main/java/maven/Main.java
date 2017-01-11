b2b0028a248457f6c2bb64d4bea802414b71077b
Dmitri Pisarenko <dp@altruix.co>
2017-01-11 19:56:11 +0300

    Attempt at fixing CSRF/CORS problem.

--- src/main/java/maven/Main.java
+++ src/main/java/maven/Main.java
@@ -2,6 +2,10 @@ package maven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by pisarenko on 11.01.2017.
@@ -11,7 +15,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Main.class, args);
     }
-/*
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
@@ -24,5 +28,4 @@ public class Main {
             }
         };
     }
-*/
 }
